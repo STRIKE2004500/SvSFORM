89a1688d821ff85f6bc7c6643effd0f62bbfd4c7
STRIKE2004500 <103489833+STRIKE2004500@users.noreply.github.com>
2025-04-25 07:41:14 +0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+
+# Connect to Google Sheets
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+# Read from a worksheet
+df = conn.read(worksheet="WOS battle")
+
+# Display the dataframe
+st.dataframe(df)
+
+
+
